9f70cd8002e6490bd71243c31542bda8a0d24d5f
douddi <doukhan.alexandre@gmail.com>
2016-12-09 23:18:19 +0100

    Sunny

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+$('.online-button').click(function () {
+  $('.online-button').addClass('to-opacity-0');
+  $('.offline-button').addClass('to-opacity-0');
+  $('body').addClass('to-overflow-auto');
 
+  $('.sun-popup').addClass('opened-popup');
+  $('.online-popup').addClass('to-opacity-1');
+});
