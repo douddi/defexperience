9b35b1417b76fffe6d903e5ebc8964ede2a6d647
douddi <doukhan.alexandre@gmail.com>
2016-12-14 19:48:02 +0100

    password edit

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ $('.online-button').click(function () {
   $('.sun-popup').addClass('opened-popup');
   $('.online-popup').addClass('to-opacity-1');
 });
+
+$('.offline-button').click(function () {
+  $('.online-button').addClass('to-opacity-0');
+  $('.offline-button').addClass('to-opacity-0');
+  $('body').addClass('to-overflow-auto');
+
+  $('.sun-popup').addClass('opened-popup');
+  $('.offline-popup').addClass('to-opacity-1');
+});
+
+$('#password-field').keypress(function(e) {
+    if(e.which == 13) {
+    	if($('#password-field').val() == 'douddi') {
+        	$('.password-wrapper').addClass('to-display-none');
+        	$('.private').addClass('to-opacity-1');
+    	} else {
+    		$('#password-field').val('');
+    	}
+    }
+});
